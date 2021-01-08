9aae9fe0231c25992c1072c45de56d0d3aa3e081
MarcGirard <girard.marc07@hotmail.fr>
2021-01-08 18:24:25 +0100

    minor changes update map

--- navigation/script/update_map.py
+++ navigation/script/update_map.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+
+import rospy
 import os
 
-copy_path = "cp " + args[1] + " $HOME/.pal/tiago_maps/configurations"
-os.system(copy_path)
-os.system("rosservice call /pal_map_manager/change_map \"input: \'salle_groix\'\"")
+#start node
+if __name__ == '__main__':
+	rospy.init_node('update_map_rviz')
+	copy_path = "cp " + args[1] + " $HOME/.pal/tiago_maps/configurations"
+	os.system(copy_path)
+	os.system("rosservice call /pal_map_manager/change_map \"input: \'salle_groix\'\"")
